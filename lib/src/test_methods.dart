72c42a90d554c8ceb0099b76ea289acc68a229d8
Andrew Brogdon <redbrogdon@google.com>
2019-06-25 16:18:03 -0700

    Added real code

---
+++ lib/src/test_methods.dart
@@ -0,0 +1,190 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'dart:async';
+
+import 'package:matcher/matcher.dart';
+
+import 'matchers.dart';
+
+/// The result of a single test.
+class TestResult {
+  const TestResult(this.success, this.messages);
+
+  final bool success;
+  final List<String> messages;
+}
+
+/// A hint that should be shown to the user upon failing an [expect] check, if
+/// [unwrappedMatcher] matches the failing value
+class PossibleHint {
+  const PossibleHint(this.unwrappedMatcher, this.message);
+
+  final dynamic unwrappedMatcher;
+  final String message;
+}
+
+/// Error thrown by [expect] when its matcher fails to match an item.
+class TestFailure {
+  const TestFailure(this.log, [this.hint = '']);
+
+  final String log;
+  final String hint;
+}
+
+/// A function under test by the [test] method.
+typedef TestableFunction = Future<void> Function();
+
+/// Logs a string in an unspecified way (Dart's print method, for instance).
+typedef LogFunction = void Function(String msg);
+
+/// A function created by [test], which will execute a single test and return the
+/// result.
+typedef TestRunnerFunction = Future<TestResult> Function();
+
+/// A function that reports test results to the user.
+typedef TestReportFunction = void Function(List<TestResult>);
+
+// A default TestReportFunction that prints a basic summary.
+void _reportResults(List<TestResult> results) {
+  print('REPORTED RESULTS:');
+  results?.forEach(
+      (result) => print('RESULT ${result.success} -- ${result.messages}'));
+}
+
+/// Executes a group of [test]-created [TestRunnerFunction]s serially, then
+/// reports the results via [reporter].
+Future<void> testGroup(
+  String description,
+  List<TestRunnerFunction> tests, {
+  TestReportFunction reporter = _reportResults,
+}) async {
+  final results = <TestResult>[];
+
+  for (final test in tests) {
+    results.add(await test());
+  }
+
+  reporter(results);
+}
+
+/// Creates a single test that will execute [fn]. Test authors should use
+/// [expect] to check for success/failure inside the functions passed as [fn].
+/// Any errors thrown during [fn]'s execution will be treated as failed tests.
+/// If nothing is thrown, the test is considered successful.
+TestRunnerFunction test(
+  String description,
+  TestableFunction fn, {
+  String successMessage = 'Test passed!',
+  String defaultHintMessage =
+      'Not quite right. Keep trying, and check the console for details.',
+  LogFunction logFn = print,
+}) {
+  assert(description != null);
+  assert(fn != null);
+  assert(successMessage != null);
+  assert(defaultHintMessage != null);
+  assert(logFn != null);
+
+  return () => runZoned(() async {
+        logFn('TEST: $description');
+
+        try {
+          await fn();
+        } on TestFailure catch (failure) {
+          logFn('TEST FAILED - ${failure.hint}');
+          logFn(failure.log);
+          return TestResult(false, [failure.hint]);
+        } catch (err) {
+          logFn('TEST FAILED');
+          logFn('Error thrown by test: $err');
+          return TestResult(false, [
+            'An error was thrown by the test. Check '
+                'the console for details.'
+          ]);
+        }
+
+        logFn('TEST PASSED');
+        return TestResult(true, [successMessage]);
+      }, onError: (e, st) {
+        // It's possible that some unawaited async action spawned during the
+        // executions of [fn] above could throw an error after [test]
+        // returns. By wrapping its body in [runZoned] and using the [onError]
+        // property, the errors can be caught and gracefully ignored.
+      });
+}
+
+/// Checks [item] against [unwrappedSuccessMatcher] and an optional list of
+/// [PossibleHint]s to determine whether a test should continue toward success
+/// or immediately fail.
+///
+/// This function is intended to be executed inside functions given to [test] as
+/// its [fn] argument. If [unwrappedSuccessMatcher] matches [item], the Future
+/// returned by [expect] completes without an an error, allowing [fn] to
+/// continue executing (as it should during a successful test).
+///
+/// If [unwrappedSuccessMatcher] does not match [item], [item] is tested against
+/// the [unwrappedMatcher] in each [PossibleHint]. As soon as one matches, a
+/// [TestFailure] is thrown with the matching hint message.
+///
+/// Functions calling [expect] *must* await the Future it returns to guarantee
+/// [expect] has a chance to check for both success and failure.
+Future<void> expect(
+  dynamic item,
+  dynamic unwrappedSuccessMatcher, {
+  List<PossibleHint> hints = const [],
+}) async {
+  final successMatcher = (unwrappedSuccessMatcher is AsyncMatcher)
+      ? unwrappedSuccessMatcher
+      : MatcherToAsyncMatcherAdapter(unwrappedSuccessMatcher);
+
+  // Attempt to match the result against the success matcher.
+  final successResults = await successMatcher.matchAsync(item);
+
+  if (!successResults.isMatch) {
+    final matcherDescription = StringDescription();
+    final mismatchDescription = StringDescription();
+
+    successMatcher.describe(matcherDescription);
+    successMatcher.describeMismatch(
+        item, mismatchDescription, successResults.matchState, false);
+
+    final logMsg = formatFailure(matcherDescription, item, mismatchDescription);
+    var hintMsg = '';
+
+    // Result wasn't successful, so find the first possible hint that
+    // matches.
+    for (final hint in hints) {
+      final hintMatcher = (hint.unwrappedMatcher is AsyncMatcher)
+          ? hint.unwrappedMatcher as AsyncMatcher
+          : MatcherToAsyncMatcherAdapter(hint.unwrappedMatcher);
+      final matchResult = await hintMatcher.matchAsync(item);
+
+      if (matchResult.isMatch) {
+        hintMsg = hint.message;
+        break;
+      }
+    }
+
+    throw TestFailure(logMsg, hintMsg);
+  }
+}
+
+String formatFailure(Description matcherDescription, dynamic expected,
+    Description mismatchDescription) {
+  final buffer = StringBuffer();
+  final pretty = StringDescription().addDescriptionOf(expected).toString();
+
+  buffer.writeln('Expected: $matcherDescription');
+  buffer.writeln('  Actual: $pretty');
+
+  if (mismatchDescription.length > 0) {
+    buffer.writeln('   Which: ${mismatchDescription.toString()}');
+  }
+
+  return buffer.toString();
+}
