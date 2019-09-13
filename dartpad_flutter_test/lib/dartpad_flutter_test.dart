589136a0f8fc5f52a033d5a31ee2d80b8da7c4f3
Andrew Brogdon <redbrogdon@google.com>
2019-09-13 13:53:35 -0700

    Updated Flutter side

--- dartpad_flutter_test/lib/dartpad_flutter_test.dart
+++ dartpad_flutter_test/lib/dartpad_flutter_test.dart
@@ -1,7 +1,7 @@
+// Copyright (c) 2019, the Flutter authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 library dartpad_flutter_test;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/test_methods.dart';
--- dartpad_flutter_test/test/dartpad_flutter_test_test.dart
+++ dartpad_flutter_test/test/dartpad_flutter_test_test.dart
@@ -1,13 +1,60 @@
+// Copyright (c) 2019, the Flutter authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dartpad_flutter_test/dartpad_flutter_test.dart';
+import 'package:dartpad_test/dartpad_test.dart' as dpt;
+import 'package:dartpad_flutter_test/dartpad_flutter_test.dart' as dft;
+
+Widget _buildWidget() {
+  return MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: MyWidget(),
+      ),
+    ),
+  );
+}
+
+class MyWidget extends StatefulWidget {
+  @override
+  _MyWidgetState createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text('$count'),
+        RaisedButton(
+          onPressed: () => setState(() => count++),
+          child: Text('Clicky!'),
+        ),
+      ],
+    );
+  }
+}
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  dft.widgetTest(
+    'asdf',
+    _buildWidget(),
+    (controller) async {
+      await controller.tap(find.byType(RaisedButton));
+      await controller.pump(Duration(seconds: 10));
+      await dpt.expect(find.text('0'), findsOneWidget);
+    },
+    logFn: (s) {
+      print('LOGGED: $s');
+    },
+  )().then((result) {
+    print(result.success);
+    print(result.messages);
   });
 }
