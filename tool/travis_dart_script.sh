4ce7ddd2604c85bbf472381f5d9223c6d41842c3
Andrew Brogdon <redbrogdon@google.com>
2020-04-20 18:31:30 -0700

    Adding Dart tests

---
+++ tool/travis_dart_script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+declare -ar PROJECT_NAMES=(
+    "dartpad_test" \
+)
+
+for PROJECT_NAME in "${PROJECT_NAMES[@]}"
+do
+    echo "== Testing '${PROJECT_NAME}'"
+    pushd "${PROJECT_NAME}"
+
+    # Run the analyzer to find any static analysis issues.
+    dartanalyzer --fatal-infos --fatal-warnings ./
+
+    # Run the formatter on all the dart files to make sure everything's linted.
+    dartfmt -n ./lib --set-exit-if-changed
+
+    # Run the actual tests.
+    pub run test test/dartpad_test_test.dart
+
+    popd
+done
+
+echo "-- Success --"
